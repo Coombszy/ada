bea6991d5c0a5fa82be7b5e34a0fad7fa5961cef
Coombszy <LCCoombs@hotmail.co.uk>
2020-03-23 22:59:47 +0000

    Added Worker Class Added Threading support for Workers

--- ada.rest/Program.cs
+++ ada.rest/Program.cs
@@ -4,6 +4,7 @@ using System.IO;
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
+using System.Threading;
 
 namespace ada.rest
 {
@@ -38,39 +39,16 @@ namespace ada.rest
 
             while (running)
             {
-                // Get context
                 HttpListenerContext context = listener.GetContext();
 
-                HttpListenerRequest request = context.Request;
-
-                string requestContents;
-                using (Stream receiveStream = request.InputStream)
-                {
-                    using (StreamReader readStream = new StreamReader(receiveStream, Encoding.UTF8))
-                    {
-                        requestContents = readStream.ReadToEnd();
-                    }
-                }
-                // Debug output
-                Debugger.Write($"Received request for {request.Url}", 5);
-                Debugger.Write($"Request headers: {request.Headers}", 5);
-                Debugger.Write($"Request contents: {requestContents}", 5);
-
-                // Obtain a response object
-                HttpListenerResponse response = context.Response;
-
-                // Construct a response
-                string responseString = "<HTML><BODY> Hello world!</BODY></HTML>";
-                byte[] buffer = Encoding.UTF8.GetBytes(responseString);
-
-                // Get a response stream and write the response to it
-                response.ContentLength64 = buffer.Length;
-                Stream output = response.OutputStream;
-                output.Write(buffer, 0, buffer.Length);
-
-                // close the output stream
-                output.Close();
+                Thread newThread = new Thread(createWorker);
+                newThread.Start(context);
             }
+        }
+
+        private static void createWorker(Object listener)
+        {
+            Worker worker = new Worker((HttpListenerContext)listener);
         }
     }
 }
---
+++ ada.rest/Worker.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Threading;
+using System.Text;
+
+namespace ada.rest
+{
+    class Worker
+    {
+        public Worker(HttpListenerContext context)
+        {
+            Debugger.Write($"Worker Thread Started ID:{Thread.CurrentThread.ManagedThreadId}");
+
+            // Get context
+            HttpListenerRequest request = context.Request;
+
+            string requestContents;
+            using (Stream receiveStream = request.InputStream)
+            {
+                using (StreamReader readStream = new StreamReader(receiveStream, Encoding.UTF8))
+                {
+                    requestContents = readStream.ReadToEnd();
+                }
+            }
+            // Debug output
+            Debugger.Write($"Received request for {request.Url}", 5);
+            Debugger.Write($"Request headers: {request.Headers}", 5);
+            Debugger.Write($"Request contents: {requestContents}", 5);
+
+            // Obtain a response object
+            HttpListenerResponse response = context.Response;
+
+            // Construct a response
+            string responseString = "<HTML><BODY> Hello world!</BODY></HTML>";
+            byte[] buffer = Encoding.UTF8.GetBytes(responseString);
+
+            // Get a response stream and write the response to it
+            response.ContentLength64 = buffer.Length;
+            Stream output = response.OutputStream;
+            output.Write(buffer, 0, buffer.Length);
+
+            // close the output stream
+            output.Close();
+            Debugger.Write($"Worker Thread closing ID:{Thread.CurrentThread.ManagedThreadId}");
+        }
+    }
+}
