d5374eb5c25fb7c6f595930fc9a996a157ee00a7
Coombszy <LCCoombs@hotmail.co.uk>
2020-03-23 19:06:53 +0000

    Update Debugger to add logging levels

--- ada.rest/Debugger.cs
+++ ada.rest/Debugger.cs
@@ -1,12 +1,11 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace ada.rest
 {
     class Debugger
     {
         private static bool debugging = false; //if debugging is enabled or disabled, default false
+        private static int debugging_level = 1; //debugging level, 1=FATAL | 5=DEBUG, default 1
 
         /// <summary>
         /// Disable and enable debuggins
@@ -19,9 +18,17 @@ namespace ada.rest
         {
             Debugger.debugging = false;
         }
+        /// <summary>
+        /// Sets the debugger level
+        /// </summary>
+        /// <param name="level"></param>
+        public static void setLevel(int level)
+        {
+            Debugger.debugging_level = level;
+        }
 
         /// <summary>
-        /// Writes out to the console if debugging is enabled
+        /// Writes out to the console if debugging is enabled - level agnostic (USE CAREFULLY)
         /// </summary>
         /// <param name="toWrite"></param>
         /// <returns></returns>
@@ -41,6 +48,24 @@ namespace ada.rest
                 }
             }
             return false;
+        }
+
+        /// <summary>
+        /// Write out to the console if debugging is enabled and level is low enough  (1=FATAL | 2=MAJOR | 3=MINOR | 4=WARN | 5=DEBUG)
+        /// </summary>
+        /// <param name="toWrite"></param>
+        /// <param name="loggingLevel"></param>
+        /// <returns></returns>
+        public static bool Write(object toWrite, int loggingLevel)
+        {
+            if(loggingLevel <= debugging_level)
+            {
+                return Write(toWrite);
+            }
+            else
+            {
+                return false;
+            }
         }
 
         /// <summary>
--- ada.rest/Program.cs
+++ ada.rest/Program.cs
@@ -7,9 +7,21 @@ namespace ada.rest
     {
         static void Main(string[] args)
         {
-            Debugger.Write("TEST1");
+            Debugger.Write("TEST");
             Debugger.enabledDebug();
-            Debugger.Write("TEST2");
+            Debugger.Write("1TEST1", 1);
+            Debugger.Write("1TEST2", 2);
+            Debugger.Write("1TEST3", 3);
+            Debugger.Write("1TEST4", 4);
+            Debugger.Write("1TEST5", 5);
+
+            Debugger.setLevel(2);
+            Debugger.Write("2TEST1", 1);
+            Debugger.Write("2TEST2", 2);
+            Debugger.Write("2TEST3", 3);
+            Debugger.Write("2TEST4", 4);
+            Debugger.Write("2TEST5", 5);
+
             Console.ReadLine();
         }
     }
