4321d2f488c6c07751c8dec330c128c0cf66b380
Coombszy <LCCoombs@hotmail.co.uk>
2020-03-23 22:20:12 +0000

    Added HTTP listener

--- ada.rest/Program.cs
+++ ada.rest/Program.cs
@@ -1,5 +1,9 @@
 ﻿using System;
-
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
 
 namespace ada.rest
 {
@@ -7,22 +11,66 @@ namespace ada.rest
     {
         static void Main(string[] args)
         {
-            Debugger.Write("TEST");
+            #region DEBUG
             Debugger.enableDebug();
-            Debugger.Write("1TEST1", 1);
-            Debugger.Write("1TEST2", 2);
-            Debugger.Write("1TEST3", 3);
-            Debugger.Write("1TEST4", 4);
-            Debugger.Write("1TEST5", 5);
+            Debugger.setLevel(5);
+            #endregion
 
-            Debugger.setLevel(2);
-            Debugger.Write("2TEST1", 1);
-            Debugger.Write("2TEST2", 2);
-            Debugger.Write("2TEST3", 3);
-            Debugger.Write("2TEST4", 4);
-            Debugger.Write("2TEST5", 5);
+            Debugger.Write("ada.api MAIN() ENTRY", 5);
 
-            Console.ReadLine();
+            // Main while loop conditional
+            bool running = true;
+
+            // URI prefixes are required
+            var prefixes = new List<string>() { "http://*:40401/" };
+
+            // Create a listener
+            HttpListener listener = new HttpListener();
+
+            // Add the prefixes
+            foreach (string s in prefixes)
+            {
+                listener.Prefixes.Add(s);
+            }
+            listener.Start();
+
+            Debugger.Write("HTTPListener is now Listening", 5);
+
+            while (running)
+            {
+                // Get context
+                HttpListenerContext context = listener.GetContext();
+
+                HttpListenerRequest request = context.Request;
+
+                string requestContents;
+                using (Stream receiveStream = request.InputStream)
+                {
+                    using (StreamReader readStream = new StreamReader(receiveStream, Encoding.UTF8))
+                    {
+                        requestContents = readStream.ReadToEnd();
+                    }
+                }
+                // Debug output
+                Debugger.Write($"Received request for {request.Url}", 5);
+                Debugger.Write($"Request headers: {request.Headers}", 5);
+                Debugger.Write($"Request contents: {requestContents}", 5);
+
+                // Obtain a response object
+                HttpListenerResponse response = context.Response;
+
+                // Construct a response
+                string responseString = "<HTML><BODY> Hello world!</BODY></HTML>";
+                byte[] buffer = Encoding.UTF8.GetBytes(responseString);
+
+                // Get a response stream and write the response to it
+                response.ContentLength64 = buffer.Length;
+                Stream output = response.OutputStream;
+                output.Write(buffer, 0, buffer.Length);
+
+                // close the output stream
+                output.Close();
+            }
         }
     }
 }
