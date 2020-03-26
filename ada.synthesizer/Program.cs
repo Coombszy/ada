c91ea37d2f377b2e13c06045f15a2f1c368c0a14
Coombszy <LCCoombs@hotmail.co.uk>
2020-03-26 00:58:42 +0000

    Add synthesizer project

---
+++ ada.synthesizer/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Threading;
+
+namespace ada.synthesizer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region DEBUG
+            Debugger.enableDebug();
+            Debugger.setLevel(5);
+            #endregion
+
+            Debugger.Write("ada.synthesizer MAIN() ENTRY", 5);
+
+            // Main while loop conditional
+            bool running = true;
+
+            // URI prefixes are required
+            var prefixes = new List<string>() { "http://*:40402/" };
+
+            // Create a listener
+            HttpListener listener = new HttpListener();
+
+            // Add the prefixes
+            foreach (string s in prefixes)
+            {
+                listener.Prefixes.Add(s);
+            }
+            listener.Start();
+
+            Debugger.Write("HTTPListener is now Listening", 5);
+
+            while (running)
+            {
+                HttpListenerContext context = listener.GetContext();
+
+                Thread newThread = new Thread(createWorker);
+                newThread.Start(context);
+            }
+        }
+
+        private static void createWorker(Object listener)
+        {
+            Worker worker = new Worker((HttpListenerContext)listener);
+        }
+    }
+}
